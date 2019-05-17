fd38fbf3bcc3f076e75f636739797eb6f235438d
eddywalls <eddieparedes12@gmail.com>
2019-05-17 00:17:58 -0400

    initial solution, need to work on base cases and change return values

--- goesToDoc/src/com/kattis/goesToDoc/Main.java
+++ goesToDoc/src/com/kattis/goesToDoc/Main.java
@@ -2,7 +2,15 @@ package com.kattis.goesToDoc;
 
 public class Main {
 
-    public static void main(String[] args) {
-	    System.out.println("Hello World!");
+    public static void main(String[] args){
+        System.out.println(isGoing("aaah", "aaaaaaah"));
+    }
+
+    public static boolean isGoing(String a, String b){
+        if(a.length() < b.length()){
+            return false;
+        }
+        else
+            return true;
     }
 }
